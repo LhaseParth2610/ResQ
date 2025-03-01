65e0c432a8ae2031600a6cc119965edaabb798fd
LhaseParth2610 <parthlhase49@gmail.com>
2025-03-01 21:42:43 +0530

    Delete debug.py

--- debug.py
+++
@@ -1,7 +0,0 @@
-from app import app, session, current_user
-from flask import session
-from flask_login import current_user
-
-with app.app_context():
-    print(session)  # Check if session data is stored
-    print(current_user.is_authenticated)  # Should be True after login
